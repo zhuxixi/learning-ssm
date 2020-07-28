c5018132e6677aa60e2c3100a3f488a6a051ba32
zhuzhenxi <zhuzhenxi_555@hotmail.com>
2020-07-28 15:06:23 +0800

    增加登录页面

---
+++ src/main/java/basics/b7/LoginServlet.java
@@ -0,0 +1,32 @@
+package basics.b7;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+
+public class LoginServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println(123);
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println(123);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- src/test/java/spring/junit/UserPwdCheckerMockTest.java
+++ src/test/java/spring/junit/UserPwdCheckerMockTest.java
@@ -2,12 +2,10 @@ package spring.junit;
 
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,27 +13,21 @@ import spring.common.CommonUtil;
 import spring.junit.mock.MockJunitTestingConfig;
 import spring.junit.mock.UserPwdChecker;
 import spring.junit.mock.UserService;
-import spring.junit.mock.UserServiceImpl;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = MockJunitTestingConfig.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = MockJunitTestingConfig.class)
 public class UserPwdCheckerMockTest {
 
-    @Mock
     public UserService userService;
 
-    @InjectMocks
-    @Autowired
     public UserPwdChecker userPwdChecker;
 
     public void before(){
     }
 
 
-    @Test
     public void mockTest(){
         CommonUtil.ps(userService.queryPwdBySaleNo("12010600000621"));
         when(userService.queryPwdBySaleNo("12010600000621")).thenReturn("Clic1234!");
